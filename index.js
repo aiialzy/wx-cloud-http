6cdf60229ae9803a2dbb897f6672a28c24732a7b
aiialzy <877090969@qq.com>
2020-03-03 20:44:20 +0800

    导出database部分API

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const fileManage = require('./src/fileManage');
 const common = require('./src/common');
 const cloudFunction = require('./src/cloudFunction');
+const database = require('./src/database');
 
 module.exports = {
-  ...common,
-  ...fileManage,
-  ...cloudFunction,
+  common,
+  fileManage,
+  cloudFunction,
+  database,
 };
